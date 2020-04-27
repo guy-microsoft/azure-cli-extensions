13f4f3e0e3b0d75620c195777f7d4c3d3e9db8ec
SDK Automation <sdkautomation@microsoft.com>
2020-04-27 10:35:59 +0000

    Update from master

--- src/portal/azext_portal/vendored_sdks/portal/aio/_portal_async.py
+++ src/portal/azext_portal/vendored_sdks/portal/aio/_portal_async.py
@@ -8,7 +8,7 @@
 
 from typing import Any, Optional
 
-from azure.core import AsyncPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 from ._configuration_async import PortalConfiguration
@@ -21,11 +21,11 @@ class Portal(object):
     """Allows creation and deletion of Azure Shared Dashboards.
 
     :ivar operation: OperationOperations operations
-    :vartype operation: portal.aio.operations_async.OperationOperations
+    :vartype operation: azure.mgmt.portal.aio.operations_async.OperationOperations
     :ivar dashboard: DashboardOperations operations
-    :vartype dashboard: portal.aio.operations_async.DashboardOperations
+    :vartype dashboard: azure.mgmt.portal.aio.operations_async.DashboardOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The Azure subscription ID. This is a GUID-formatted string (e.g. 00000000-0000-0000-0000-000000000000).
     :type subscription_id: str
     :param str base_url: Service URL
@@ -33,7 +33,7 @@ class Portal(object):
 
     def __init__(
         self,
-        credential: "TokenCredential",
+        credential: "AsyncTokenCredential",
         subscription_id: str,
         base_url: Optional[str] = None,
         **kwargs: Any
@@ -41,7 +41,7 @@ class Portal(object):
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = PortalConfiguration(credential, subscription_id, **kwargs)
-        self._client = AsyncPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
